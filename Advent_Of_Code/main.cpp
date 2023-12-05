f9ca8da1d2ddc0f24617c2d5b1110d0eace8e045
Ethanil <ruben.gielisch@gmail.com>
2023-12-05 15:14:50 +0100

    fixed stuff

--- Advent_Of_Code/main.cpp
+++ Advent_Of_Code/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <chrono>
-#include "day1.hpp"
-#include "day2.hpp"
-#include "day3.hpp"
-#include "day4.hpp"
-#include "day5.hpp"
+#include "day1/day1.hpp"
+#include "day2/day2.hpp"
+#include "day3/day3.hpp"
+#include "day4/day4.hpp"
+#include "day5/day5.hpp"
 double getTime(long long (*func)(const char*), const char* filename, int times = 10000) {
 	double sum = 0;
 	for (int i = 0; i < times; ++i) {
@@ -16,6 +16,6 @@ double getTime(long long (*func)(const char*), const char* filename, int times =
 	return sum / times;
 }
 int main() {
-	std::cout << getTime(day5::calc2, "day5.txt") <<" microseconds";
+	std::cout << getTime(day5::calc2, "day5/day5.txt") <<" microseconds";
 	//std::cout << day5::calc2("day5.txt");
 }
